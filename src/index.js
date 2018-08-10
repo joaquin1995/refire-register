1daae6a8140e64d8f1d0f8f6d6a72f91c81bdf95
joaquin <joakincortez8@gmail.com>
2018-08-10 17:42:47 -0400

    'addPickers'

--- src/index.js
+++ src/index.js
@@ -2,14 +2,19 @@ import React,{ Component} from 'react'
 import ReactDOM from 'react-dom'
 import Typography from '@material-ui/core/Typography'
 import Paper from '@material-ui/core/Paper'
+import TextField from '@material-ui/core/TextField'
 import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button'
+import MenuItem from '@material-ui/core/MenuItem'
+import { Menu } from '../node_modules/@material-ui/core';
 
 const styles = (theme) =>{
    return{
         container:
         {
         width: '500px',
-        margin: '0 auto'
+        margin: '0 auto',
+        padding : theme.spacing.unit * 2
         }
     }
 }
@@ -24,6 +29,52 @@ class App extends Component {
         <Typography variant="display1" 
         color = 'secondary'
         >Registro de la comunidad</Typography>
+        <TextField
+        label='Nombre'
+        name='firsName'
+        fullWidth
+        />
+        <TextField
+        label='Apellido'
+        name='firsA'
+        fullWidth
+        />
+        <TextField
+        label='Celular'
+        name='Phone'
+        fullWidth
+        />
+        <TextField
+        label='Email'
+        name='Email'
+        fullWidth
+        />
+         <TextField
+        label='Facebook'
+        name='Facebook'
+        fullWidth
+        />
+         <TextField
+        label='FechaNacimiento'
+        name='Naciminto'
+        type='Date'
+        fullWidth
+        />
+        <TextField
+        label='Interes'
+        name='Interes'
+        type='Date'
+        fullWidth
+        select
+        >
+        <MenuItem>Javascript</MenuItem>
+        <MenuItem>FS</MenuItem>
+        <MenuItem>HTML5</MenuItem>
+        <MenuItem>React</MenuItem>
+        </TextField>
+        <Button  variant="contained" color="secondary" className={classes.container}>
+        Guardar
+        </Button>
         </Paper>
         )
     }
