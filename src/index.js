43248b9afda9211aaa1f94f204bd6ca1c11498c4
joaquin <joakincortez8@gmail.com>
2018-08-10 17:17:15 -0400

    'addTitle'

--- src/index.js
+++ src/index.js
@@ -1,15 +1,34 @@
 import React,{ Component} from 'react'
 import ReactDOM from 'react-dom'
+import Typography from '@material-ui/core/Typography'
+import Paper from '@material-ui/core/Paper'
+import { withStyles } from '@material-ui/core/styles';
 
+const styles = (theme) =>{
+   return{
+        container:
+        {
+        width: '500px',
+        margin: '0 auto'
+        }
+    }
+}
 
 
 class App extends Component {
     render(){
+        const classes = this.props.classes
+
         return(
-        <h1>Registro de la comunidad</h1>
+        <Paper className={classes.container}>
+        <Typography variant="display1" 
+        color = 'secondary'
+        >Registro de la comunidad</Typography>
+        </Paper>
         )
     }
 }
+App = withStyles(styles)(App)
 
 ReactDOM.render(
     <App />,
